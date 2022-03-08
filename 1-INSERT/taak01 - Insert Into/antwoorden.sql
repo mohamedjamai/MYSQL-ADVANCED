cbcd619365b8a5cb31e4722f0455511470d26861
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-03-08 14:36:41 +0100

    antwoord

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,2 @@
-
+INSERT INTO student (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('7', 'Mohamed', 'Jamai', '1B', 'Karel', '1061AJ', '12-10-2004', 'Amsterdam');
