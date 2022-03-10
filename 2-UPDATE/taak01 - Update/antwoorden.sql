9aa07ae357c9a5592dfb05a7605855f527470d7b
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-03-10 12:46:05 +0100

    update

--- 2-UPDATE/taak01 - Update/antwoorden.sql
+++ 2-UPDATE/taak01 - Update/antwoorden.sql
@@ -6,7 +6,7 @@ UPDATE studenten2 SET adres= ' derikstraat', postcode= '1063AJ' WHERE student_id
 UPDATE studenten2 SET adres= ' Clerqstraat', postcode= '1056AJ' WHERE student_id = 10;
 
 -- Opdracht 3
-UPDATE studenten2 SET geboortedatum= '12-12-2000' WHERE student_id= 2;
+UPDATE studenten2 SET geboortedatum= '2000-12-12' WHERE student_id= 2;
 -- Opdracht 4
 UPDATE studenten2 SET klas= '9C' WHERE student_id= 4;
 UPDATE studenten2 SET klas= '9A' WHERE student_id= 8;
